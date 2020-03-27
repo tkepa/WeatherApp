f4c5ba66fddd9c6e2eda248f8f76f3f6aa3dd685
Tomasz Kępa <tomaszkepa10@gmail.com>
2020-03-27 01:43:22 +0100

    working on gelocation

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+"use strict";
+
+// service worker registration - remove if you're not going to use it
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+// place your code below
+
+function success(loc) {
+  const {coords} = loc;
+  
+}; 
+  
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+}
+
+function consolValue(val) {
+  return val;
+}
+
+function location(){
+  navigator.geolocation.getCurrentPosition(success, error, options);
+}
+
+location();
+console.log(position);
