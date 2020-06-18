fcbfffbd0ba96f02d3fdf428a3ffc18d927bda36
Tomasz Kępa <tomaszkepa10@gmail.com>
2020-06-18 13:52:16 +0200

    FEAT: Added Site title

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# wtf-gulp-starter
-
-`npm install -g gulp-cli`
-
-`npm install`
-
-`gulp`
-
-To publish your page using github pages use `npm run deploy`
 # WeatherApp
--- src/js/main.js
+++ src/js/main.js
@@ -87,217 +87,233 @@ async function getLocation() {
   weatherByPosition = await weatherByPosition.json();
   weatherByHour = await weatherByHour.json();
   weatherByHour = weatherByHour.hourly;
-  const hourOfWeather = mapAndFilter(weatherByHour.map((obj) => obj.dt));
-  let temperature = filterTemperature(weatherByHour);
-  const temperatureCoords = temperatureToChartCoords(temperature);
+  const chart = new Chart();
+  const hourOfWeather = chart.mapAndFilter(weatherByHour.map((obj) => obj.dt));
+  let temperature = chart.filterTemperature(weatherByHour);
+  const temperatureCoords = chart.temperatureToChartCoords(temperature);
   
-  const tempPixels = temperatureToPixel(temperatureCoords);
-  const hoursPixels = hoursToPixel(getHoursCoords());
+  const tempPixels = chart.temperatureToPixel(temperatureCoords);
+  const hoursPixels = chart.hoursToPixel(chart.getHoursCoords());
   
-  const tempPoints = createTempObj(hoursPixels, tempPixels, hourOfWeather, temperature);
+  const tempPoints = chart.createTempObj(hoursPixels, tempPixels, hourOfWeather, temperature);
 
   
   
 
   
-  
-  grid();
-  drawAxis(hourOfWeather);
-  await wait(500);
-  drawChart(temperatureCoords)
-  addPopUp(tempPoints);
+  chart.canvasDimensions();
+  chart.drawGrid();
+  chart.drawAxis(hourOfWeather);
+  await chart.waitForGrid(500);
+  chart.drawChart(temperatureCoords)
+  chart.addPopUp(tempPoints);
 }
 
 
 
-const canvas = document.getElementById("chartTemp__canvas");
-canvas.width = 660;
-canvas.height = 309;
-const ctx = canvas.getContext("2d");
 
-function grid() {
-  let gridX = 40;
-  let gridY = 40;
-  const cellSize = 40;
-  ctx.beginPath();
-  ctx.strokeStyle = "lightgrey";
 
-  while (gridX <= canvas.width - 20) {
-    ctx.moveTo(gridX, 20);
-    ctx.lineTo(gridX, canvas.height - 20);
-    gridX += cellSize;
-  }
-  while (gridY <= canvas.height - 20) {
-    ctx.moveTo(20, gridY);
-    ctx.lineTo(canvas.width - 10, gridY);
-    gridY += cellSize;
-  }
-  ctx.stroke();
-}
 
-function blocks(count) {
-  return count * 40;
-}
+class Chart {
+  constructor() {
+    this.condition = false;
+    this.state = {};
+    this.tooltip = document.getElementsByClassName("chartTemp__tooltip")[0];
+    //this.tooltip = tooltip[0];
+    this.canvas = document.getElementById("chartTemp__canvas");
 
-async function drawAxis(hours) {
-  ctx.beginPath();
-  ctx.strokeStyle = "black";
-  ctx.moveTo(blocks(1), blocks(1 / 2));
-  ctx.lineTo(blocks(1), blocks(7));
-  ctx.lineTo(blocks(16), blocks(7));
-
-  ctx.moveTo(blocks(1), blocks(7));
-  let text = 0;
-  let textY = blocks(7);
-  for (let i = 1; i <= 7; i++) {
-    ctx.strokeText(text, blocks(1 / 2), textY);
-    textY -= 40;
-    text += 5;
+    this.ctx = this.canvas.getContext("2d");
   }
 
-  ctx.moveTo(blocks(2), blocks(7));
-  let textX = blocks(1.9);
-
-  for (let i = 0; i <= 15; i++) {
-    ctx.strokeText(hours[i], textX, blocks(7.5));
-    textX += 40;
+  canvasDimensions() {
+    this.canvas.width = 660;
+    this.canvas.height = 309;
   }
-  ctx.stroke();
-}
-function timestampToTime(stamp) {
-  let date = new Date(stamp * 1000);
-  return date.getHours();
-}
 
-function mapAndFilter(tempArray) {
-  tempArray = tempArray.map((el) => timestampToTime(el));
-  tempArray = tempArray.filter((el) => el % 2 == 0);
-  return tempArray;
-}
+  blocks(count) {
+    return count * 40;
+  }
 
-function temperatureToChartCoords(temp){
-  let temperature = temp.map((el) => el / 5);
-  temperature = temperature.slice(0,15);
-  return temperature;
-}
-
-function getHoursCoords() {
-  let hourOfTemp = [...Array(17).keys()]
-  hourOfTemp = hourOfTemp.slice(2, 17);
-
-  return hourOfTemp;
-}
-
-function drawChart(temperature) {
-  const tempHours = getHoursCoords();
-  let i = 1;
-  function animation() {
-    if (i <= temperature.length) {
-      window.requestAnimationFrame(animation);
+  drawGrid() {
+    let gridX = 40;
+    let gridY = 40;
+    const cellSize = 40;
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = "lightgrey";
+  
+    while (gridX <= this.canvas.width - 20) {
+      this.ctx.moveTo(gridX, 20);
+      this.ctx.lineTo(gridX, this.canvas.height - 20);
+      gridX += cellSize;
     }
-    ctx.strokeStyle = "#FFE74A";
-    ctx.lineWidth = 3;
-    ctx.beginPath();
-    ctx.moveTo(blocks(tempHours[i-1]), axisXposition - blocks(temperature[i-1]));
-    ctx.arc(blocks(tempHours[i-1]), axisXposition - blocks(temperature[i-1]), 3, 0, Math.PI * 2, true);
-    ctx.lineTo(blocks(tempHours[i]), axisXposition - blocks(temperature[i]));
-    ctx.stroke();
-    i++;
+    while (gridY <= this.canvas.height - 20) {
+      this.ctx.moveTo(20, gridY);
+      this.ctx.lineTo(this.canvas.width - 10, gridY);
+      gridY += cellSize;
+    }
+    this.ctx.stroke();
   }
-  animation();
-}
 
-async function wait(ms) {
-  return new Promise(r => setTimeout(r, ms));
-}
+  async drawAxis(hours) {
+    this.ctx.beginPath();
+    this.ctx.strokeStyle = "black";
+    this.ctx.moveTo(this.blocks(1), this.blocks(1 / 2));
+    this.ctx.lineTo(this.blocks(1), this.blocks(7));
+    this.ctx.lineTo(this.blocks(16), this.blocks(7));
+  
+    this.ctx.moveTo(this.blocks(1), this.blocks(7));
+    let text = 0;
+    let textY = this.blocks(7);
+    for (let i = 1; i <= 7; i++) {
+      this.ctx.strokeText(text, this.blocks(1 / 2), textY);
+      textY -= 40;
+      text += 5;
+    }
+  
+    this.ctx.moveTo(this.blocks(2), this.blocks(7));
+    let textX = this.blocks(1.9);
+  
+    for (let i = 0; i <= 15; i++) {
+      this.ctx.strokeText(hours[i], textX, this.blocks(7.5));
+      textX += 40;
+    }
+    this.ctx.stroke();
+  }
+  timestampToTime(stamp) {
+    let date = new Date(stamp * 1000);
+    return date.getHours();
+  }
+  
+  mapAndFilter(tempArray) {
+    tempArray = tempArray.map((el) => this.timestampToTime(el));
+    tempArray = tempArray.filter((el) => el % 2 == 0);
+    return tempArray;
+  }
+  
+  temperatureToChartCoords(temp){
+    let temperature = temp.map((el) => el / 5);
+    temperature = temperature.slice(0,15);
+    return temperature;
+  }
+  
+  getHoursCoords() {
+    let hourOfTemp = [...Array(17).keys()]
+    hourOfTemp = hourOfTemp.slice(2, 17);
+  
+    return hourOfTemp;
+  }
+  
+  drawChart(temperature) {
+    const tempHours = this.getHoursCoords();
+    let i = 1;
+    const context = this.ctx
+    const block = this.blocks();
+    function animation() {
+      if (i <= temperature.length) {
+        window.requestAnimationFrame(animation);
+      }
+      context.strokeStyle = "#FFE74A";
+      context.lineWidth = 3;
+      context.beginPath();
+      context.moveTo(block(tempHours[i-1]), axisXposition - block(temperature[i-1]));
+      context.arc(block(tempHours[i-1]), axisXposition - block(temperature[i-1]), 3, 0, Math.PI * 2, true);
+      context.lineTo(block(tempHours[i]), axisXposition - block(temperature[i]));
+      context.stroke();
+      i++;
+    }
+    animation();
+  }
+  
+  async waitForGrid(ms) {
+    return new Promise(r => setTimeout(r, ms));
+  }
+  
+  getMousePosition(evt) {
+    let rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+  }
+  
+  filterTemperature(temperature) {
+    const temp = temperature
+      .map((obj) => obj.temp)
+      .filter((el, index) => index % 2 == 0);
+  
+    return temp;
+  }
+  
+  temperatureToPixel(temp){
+    temp = temp.map(el => axisXposition - this.blocks(el));
+    return temp;
+  }
+  
+  hoursToPixel(hours) {
+    const hourrs = hours.map(el => this.blocks(el));
+    return hourrs;
+  }
+  
+  createTempObj(hour, temp, hourOfTemp, tempInHour){
+    const tempPointCoords = [];
+    for (let i = 0; i < temp.length; i++){
+      tempPointCoords.push({
+        x: hour[i],
+        y: temp[i],
+        xmin: hour[i] - 3,
+        xmax: hour[i] + 3,
+        ymin: temp[i] - 3,
+        ymax: temp[i] + 3,
+        hourTemp: {
+          hour: hourOfTemp[i],
+          temp: tempInHour[i]
+        },
+  
+      })
+    }
+    return tempPointCoords;
+  }
+  
+  drawPoint(coords = {}, radius, color = 'white') {
+    const mouseCoordinate = coords;
+    this.ctx.strokeStyle = color;
+    this.ctx.beginPath();
+    this.ctx.moveTo(mouseCoordinate.x, mouseCoordinate.y);
+    this.ctx.arc(mouseCoordinate.x, mouseCoordinate.y, radius, 0, Math.PI * 2, true);
+    this.ctx.fillStyle = color;
+    this.ctx.fill();
+    this.ctx.stroke();
+  }
+  
 
-function getMousePosition(canvas, evt) {
-  let rect = canvas.getBoundingClientRect();
-  return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
-  };
-}
-
-function filterTemperature(temperature) {
-  const temp = temperature
-    .map((obj) => obj.temp)
-    .filter((el, index) => index % 2 == 0);
-
-  return temp;
-}
-
-function temperatureToPixel(temp){
-  temp = temp.map(el => axisXposition - blocks(el));
-  return temp;
-}
-
-function hoursToPixel(hours) {
-  const hourrs = hours.map(el => blocks(el));
-  return hourrs;
-}
-
-function createTempObj(hour, temp, hourOfTemp, tempInHour){
-  const tempPointCoords = [];
-  for (let i = 0; i < temp.length; i++){
-    tempPointCoords.push({
-      x: hour[i],
-      y: temp[i],
-      xmin: hour[i] - 3,
-      xmax: hour[i] + 3,
-      ymin: temp[i] - 3,
-      ymax: temp[i] + 3,
-      hourTemp: {
-        hour: hourOfTemp[i],
-        temp: tempInHour[i]
-      },
-
+  addPopUp(tempPoint) {
+    this.canvas.addEventListener('mousemove', (e) => {
+      const mouseCoords = this.getMousePosition(e);
+      let boools = tempPoint.filter(el => {
+        const a = el.xmin <= mouseCoords.x;
+        const b = mouseCoords.x <= el.xmax;
+        const c = el.ymin <= mouseCoords.y;
+        const d = mouseCoords.y <= el.ymax;
+        return (a && b && c && d)})
+      
+      if(boools.length) {
+        this.drawPoint(boools[0], 1);
+        this.state = {...boools[0]};
+        this.condition = true;
+        this.tooltip.style.left = `${this.state.x + 5}px`;
+        this.tooltip.style.top = `${this.state.y - 35}px`;
+        this.tooltip.style.display = 'flex';
+        this.tooltip.innerHTML = `Time: ${this.state.hourTemp.hour}:00, Temp: ${this.state.hourTemp.temp}°C`;
+      } 
+      else if(!boools.length) {
+        this.drawPoint(this.state, 3, "#FFE74A");
+        this.condition = false;
+        this.state = {};
+        this.tooltip.style.display = 'none';
+      } 
     })
   }
-  return tempPointCoords;
-}
-
-function drawPoint(coords = {}, radius, color = 'white') {
-  const mouseCoordinate = coords;
-  ctx.strokeStyle = color;
-  ctx.beginPath();
-  ctx.moveTo(mouseCoordinate.x, mouseCoordinate.y);
-  ctx.arc(mouseCoordinate.x, mouseCoordinate.y, radius, 0, Math.PI * 2, true);
-  ctx.fillStyle = color;
-  ctx.fill();
-  ctx.stroke();
-}
-
-let condition = false;
-let state = {};
-let tooltip = document.getElementsByClassName("chartTemp__tooltip");
-tooltip = tooltip[0]
-function addPopUp(tempPoint) {
-  canvas.addEventListener('mousemove', function(evt) {
-    const mouseCoords = getMousePosition(canvas, evt);
-    let boools = tempPoint.filter(el => {
-      const a = el.xmin <= mouseCoords.x;
-      const b = mouseCoords.x <= el.xmax;
-      const c = el.ymin <= mouseCoords.y;
-      const d = mouseCoords.y <= el.ymax;
-      return (a && b && c && d)})
-    
-    if(boools.length) {
-      drawPoint(boools[0], 1);
-      state = {...boools[0]};
-      condition = true;
-      tooltip.style.left = `${state.x + 5}px`;
-      tooltip.style.top = `${state.y - 35}px`;
-      tooltip.style.display = 'flex';
-      tooltip.innerHTML = `Time: ${state.hourTemp.hour}:00, Temp: ${state.hourTemp.temp}°C`;
-    } 
-    else if(!boools.length) {
-      drawPoint(state, 3, "#FFE74A");
-      condition = false;
-      state = {};
-      tooltip.style.display = 'none';
-    } 
-  })
+  
 }
 
 
